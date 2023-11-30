d2ca11483a7a086d12974e0e745ed629f1aafe28
Neha <147375758+0406neha@users.noreply.github.com>
2023-11-30 13:05:26 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,5 +1,30 @@
+/*
 let js = "amazing";
-if (js === "amazing") alert("JavaScript is FUN!");
-
-40 + 8 + 23 - 10;
 console.log(40 + 8 + 23 - 10);
+
+console.log("Genie");
+console.log(23);
+
+let firstName = "Berry";
+
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
+*/
+
+// Assignment practice-1
+let country = "India";
+let continent = "Asia";
+let population = "1,428.6";
+let indiaIsAReligiousCountry = true;
+
+console.log(country);
+console.log(continent);
+console.log(population);
+console.log(indiaIsAReligiousCountry);
+
+console.log(typeof true);
+console.log(typeof indiaIsAReligiousCountry);
+console.log(typeof "India");
+console.log(typeof "Asia");
+console.log(typeof 1428.6);

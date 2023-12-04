5811277c5907e38b31203c995fe93657d80feb3c
Neha <147375758+0406neha@users.noreply.github.com>
2023-12-04 13:28:12 +0530

    Add files via upload

--- Truthy & Falsy.js
+++ Truthy & Falsy.js
@@ -5,3 +5,17 @@ console.log(Boolean(undefined));
 console.log(Boolean({}));
 console.log(Boolean(""));
 console.log(Boolean("Neha"));
+
+const money = 0;
+if (money) {
+  console.log("Don't spend it all :) ");
+} else {
+  console.log("You should get a job!");
+}
+
+let height;
+if (height) {
+  console.log("YAY! Height is defined ");
+} else {
+  console.log("Height is UNDEFINED");
+}

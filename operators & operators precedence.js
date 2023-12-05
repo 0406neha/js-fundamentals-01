e0370a430229ee9dcfbc34fd90c401fec0af02f6
Neha <147375758+0406neha@users.noreply.github.com>
2023-12-05 13:16:32 +0530

    Add files via upload

--- operators & operators precedence.js
+++ operators & operators precedence.js
@@ -70,3 +70,29 @@ if (birthYear <= 2000) {
 }
 
 console.log(century);
+
+// Equality operators: == vs. ===
+
+const Age = "18";
+if (Age === 18) console.log("You just became an adult :D (strict)");
+if (Age == 18) console.log("You just became an adult :D (loose)");
+
+//Prompt window
+
+const favourite = Number(prompt("What's your favourite number?"));
+
+console.log(favourite);
+console.log(typeof favourite);
+
+if (favourite === 23) {
+  // 23 === 23 -> TRUE
+  console.log("Cool! 23 is an amazing number!");
+} else if (favourite === 7) {
+  console.log("7 is also a cool number");
+} else if (favourite === 9) {
+  console.log("9 is also a cool number");
+} else {
+  console.log("Number is not 23 or 7 or 9");
+}
+
+if (favourite !== 23) console.log("Why not 23?");
